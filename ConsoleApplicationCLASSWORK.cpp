41a67b8f6f9988136d9feb54ff33294c8db39567
maksik1488 <159796599+maksik1488@users.noreply.github.com>
2024-03-18 13:44:52 +0300

    Add files via upload

---
+++ ConsoleApplicationCLASSWORK.cpp
@@ -0,0 +1,36 @@
+﻿
+using namespace std;
+#include <iostream>
+
+int main()
+{
+	int min, max;
+	const int A = 10;
+	int array[A];
+	cin >> min >> max;
+	cout << "min and max: ";
+	setlocale(LC_ALL, "rus");
+	bool flag;
+	srand(time(NULL));
+	for (int i = 0; i < A; i++) {
+		int c = min + rand() % (max - min - 1);
+		array[i] = c;
+		cout << array[i] << " ";
+
+	}
+	for (int j = 0; j < A; j++) {
+		flag = true;
+		for (int i = 0; i < A - 1 - j; i++) {
+			if (array[i] > array[i + 1]) {
+				swap(array[i], array[i + 1]);
+				flag = false;
+			}
+
+		}
+		if (flag) break;
+	}
+	cout << endl;
+	for (int i = 0; i < A; i++) {
+		cout << array[i] << " ";
+	}
+}
